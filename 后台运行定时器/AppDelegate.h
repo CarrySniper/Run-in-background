78ae2ec16cf84d6396c38fc93b4056b5c21775dc
cjq002 <692771080@qq.com>
2015-03-11 14:25:58 +0800

    Initial Commit

---
+++ 后台运行定时器/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  后台运行定时器
+//
+//  Created by 陈家庆 on 15-3-11.
+//  Copyright (c) 2015年 shikee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 后台运行定时器Tests/_______Tests.m
@@ -0,0 +1,34 @@
+//
+//  _______Tests.m
+//  后台运行定时器Tests
+//
+//  Created by 陈家庆 on 15-3-11.
+//  Copyright (c) 2015年 shikee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _______Tests : XCTestCase
+
+@end
+
+@implementation _______Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
